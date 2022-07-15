0c484a972c89ae1a14d7b8797d7a66b8449f9d11
saradoan <sdoan2@gmu.edu>
2022-07-15 10:54:22 -0400

    updates to SDSS input to comply with expected input attributes

--- utils/input/sdss_input.py
+++ utils/input/sdss_input.py
@@ -4,52 +4,45 @@ import pathlib
 
 from utils.input.input import BadassInput, SDSS_FMT
 from utils.utils import find_nearest
+from utils.constants import C
 
 class SDSSSpec(BadassInput):
+
     @classmethod
     def read_sdss_spec(cls, input_data, options):
+        return cls(input_data)
 
+
+    def __init__(self, input_data):
         if not isinstance(input_data, pathlib.Path):
             raise Exception('Reading SDSS spectra from data currently unsupported')
 
-        spec = cls()
-        spec.infile = input_data
-
-        with fits.open(spec.infile) as hdu:
-            spec.specobj = hdu[2].data
-            spec.z = spec.specobj['z'][0]
+        super().__init__()
+        self.infile = input_data
+        with fits.open(self.infile) as hdu:
+            specobj = hdu[2].data
+            self.z = specobj['z'][0]
 
             if 'RA' in hdu[0].header:
-                spec.ra = hdu[0].header['RA']
-                spec.dec = hdu[0].header['DEC']
+                self.ra = hdu[0].header['RA']
+                self.dec = hdu[0].header['DEC']
             elif 'PLUG_RA' in hdu[0].header:
-                spec.ra = spec.specobj['PLUG_RA'][0]
-                spec.dec = spec.specobj['PLUG_DEC'][0]
+                self.ra = specobj['PLUG_RA'][0]
+                self.dec = specobj['PLUG_DEC'][0]
             else:
-                spec.ra = None
-                spec.dec = None
+                self.ra = None
+                self.dec = None
 
             t = hdu[1].data
 
             # Unpack the spectra
-            spec.flux = t['flux']
-            spec.wave = np.power(10, t['loglam'])
-            spec.lam = spec.wave / (1+spec.z)
-            spec.ivar = t['ivar']
-            spec.error = np.sqrt(1 / spec.ivar)
+            self.spec = t['flux']
+            self.wave = np.power(10, t['loglam']) / (1+self.z)
+            self.noise = np.sqrt(1 / t['ivar'])
+            self.mask = t['and_mask'] # TODO: need?
 
-        return spec
-
-
-    def validate_input(self):
-        for attr in ['z', 'ra', 'dec', 'flux', 'wave', 'error']:
-            if not hasattr(self, attr):
-                raise Exception('SDSS Spec input missing expected value: {attr}'.format(attr=attr))
-        return True
-
-
-    # def generate_mask(self):
-
-
-    # def prepare_input(self, ba_ctx):
-    #     fit_min, fit_max = ba_ctx.options.fit_options.fit_reg
+            frac = self.wave[1]/self.wave[0] # Constant lambda fraction per pixel
+            dlam_gal = (frac - 1)*self.wave # Size of every pixel in Angstrom
+            wdisp = t['wdisp'] # Intrinsic dispersion of every pixel, in pixels units
+            self.fwhm_res = 2.355*wdisp*dlam_gal # Resolution FWHM of every pixel, in angstroms
+            self.velscale = np.log(frac) * C
